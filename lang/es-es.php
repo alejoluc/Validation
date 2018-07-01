c91dd52271068cd510ce4dc048974d1e4a8eb01a
Alejo Lucangeli <alejolucangeli@gmail.com>
2018-07-01 09:56:49 -0300

    Edited spanish language

--- lang/es-es.php
+++ lang/es-es.php
@@ -2,6 +2,16 @@
 
 return [
     'NotEmpty'      => 'El campo {field} no puede estar vacío',
-    'Alphanumeric'  => 'El campo {field} solo puede contener caracteres alfanuméricos',
-    'Alpha'         => 'El campo {field} solo puede contener letras'
+    'Alphanumeric'  => 'El campo {field} solo puede contener letras y números',
+    'Alpha'         => 'El campo {field} solo puede contener letras',
+    'Numeric'       => 'El campo {field} solo puede contener números',
+    'AlphanumericUnderscore' => 'El campo {field} solo puede contener letras, números y guión bajo',
+    'Email'         => 'El campo {field} debe contener una dirección de E-mail válida',
+    'Equals'        => 'El campo {field} tiene que ser igual a {compareWith}',
+    'NotEquals'     => 'El campo {field} tiene que ser distinto a {compareWith}',
+    'LessThan'      => 'El campo {field} tiene que ser menor a {max}',
+    'MoreThan'      => 'El campo {field} tiene que ser mayor a {min}',
+    'Between'       => 'El campo {field} tiene que estar entre {min} y {max}',
+    'Regex'         => 'El campo {field} no contiene un valor válido',
+    'URL'           => 'El campo {field} debe contener una URL válida'
 ];
